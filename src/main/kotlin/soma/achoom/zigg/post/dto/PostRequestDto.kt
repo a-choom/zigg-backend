3433b259d46a4cca0f0c425844c096c4d7a2ffce
강지훈 <clone0922@gmail.com>
2024-11-12 18:07:46 +0900

    Feat : Post Request @JsonInclude

--- src/main/kotlin/soma/achoom/zigg/post/dto/PostRequestDto.kt
+++ src/main/kotlin/soma/achoom/zigg/post/dto/PostRequestDto.kt
@@ -1,9 +1,7 @@
 package soma.achoom.zigg.post.dto
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import soma.achoom.zigg.content.dto.VideoRequestDto
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class PostRequestDto(
     val postTitle: String,
     val postMessage: String,
