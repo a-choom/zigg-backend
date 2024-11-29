01e6be87ffff08ada365620de963c9823004a8aa
강지훈 <clone0922@gmail.com>
2024-11-29 11:41:39 +0900

    Fix : 차단된 유저 처리 로직 수정

--- src/main/kotlin/soma/achoom/zigg/report/dto/PostReportMetaDto.kt
+++ src/main/kotlin/soma/achoom/zigg/report/dto/PostReportMetaDto.kt
@@ -3,7 +3,8 @@ package soma.achoom.zigg.report.dto
 data class PostReportMetaDto(
     val postId:Long,
     val userId:Long,
-    val contents: List<Long>,
+    val imageContent: List<Long>,
+    val videoContent : Long?,
     val textContent : String
 ) {
 }
